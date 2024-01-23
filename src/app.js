63bd7db8b5e616ab3efb2129725b2d547bc4e303
Atila Cassel <atilacassel@gmail.com>
2024-01-23 15:33:15 +0000

    Add checkbox to complete tasks

--- src/app.js
+++ src/app.js
@@ -5,24 +5,41 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         const taskInput = document.getElementById('taskInput');
         const taskText = taskInput.value;
+
         if (taskText.trim() !== '') {
             addTask(taskText);
             taskInput.value = '';
         } else {
-            alert('Please enter a name for the task.');
+            alert('Please enter a task name.');
         }
-        
     });
 
     function addTask(taskText) {
         const taskList = document.getElementById('taskList');
         const li = document.createElement('li');
-        li.textContent = taskText;
+
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.addEventListener('click', function () {
             li.remove();
         });
+
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.addEventListener('change', function () {
+            // Adicione lógica para marcar/desmarcar a tarefa como concluída.
+            if (checkBox.checked) {
+                taskContent.classList.add('completed');
+            } else {
+                taskContent.classList.remove('completed');
+            }
+        });
+
+        const taskContent = document.createElement('span');
+        taskContent.textContent = taskText;
+
+        li.appendChild(checkBox);
+        li.appendChild(taskContent);
         li.appendChild(deleteButton);
         taskList.appendChild(li);
     }
