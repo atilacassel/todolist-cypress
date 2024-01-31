53f7f7da93dc5f0f9f792f5699ff70502ff743a2
Atila Cassel <atilacassel@gmail.com>
2024-01-31 17:22:25 +0000

    Create todoList spec with first test

--- cypress/e2e/todoList.cy.js
+++ cypress/e2e/todoList.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Todo List', () => {
+
+  beforeEach(() => {
+
+    cy.visit('./src/index.html')
+
   })
+
+  it('after visiting a page, asserts some text is visible', () => {
+
+    cy
+      .get('[data-testid="todo-list-header"]')
+      .should('be.visible')
+
+    })
 })
