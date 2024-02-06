a6fce93956abf094c6cfa7725abb2d9741e3b2ea
Atila Cassel <atilacassel@gmail.com>
2024-02-06 18:26:41 +0000

    Add second test for task creation and update names and formatting

--- cypress/e2e/todoList.cy.js
+++ cypress/e2e/todoList.cy.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
 
-describe("Todo List", () => {
+describe('Todo List', () => {
   beforeEach(() => {
-    cy.visit("./src/index.html");
-  });
+    cy.visit('./src/index.html')
+  })
 
-  it("after visiting a page, asserts some text is visible", () => {
-    cy.contains('h1', 'Your To-Do List').should("be.visible");
-  });
-});
+  it('Check if the text is visible after page load', () => {
+    cy.contains('h1', 'Your To-Do List').should('be.visible')
+  })
+
+  it('Verify creation of a new task', () => {
+    cy.get('[data-testid="task-input"]').type('New task')
+    cy.get('[data-testid="add-button"]').click()
+    cy.contains('span', 'New task').should('be.visible')
+  })
+})
